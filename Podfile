952fe2ebbf92dc301869317feb133519201771e5
RLT <dev.romain@me.com>
2015-09-11 18:15:49 +0200

    New Google Maps Podspec test

--- Podfile
+++ Podfile
@@ -2,6 +2,6 @@ source 'https://github.com/CocoaPods/Specs.git'
 
 platform :ios, '8.0'
 
-pod 'GoogleMaps'
+pod 'Google-Maps-iOS-SDK', :podspec => "https://raw.githubusercontent.com/Reflejo/GoogleMapsPodspec/master/Google-Maps-iOS-SDK.podspec.json"
 
 use_frameworks!
---
+++ Sample/PXGoogleDirectionsSample/PXGoogleDirectionsSample-BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  PXGoogleDirectionsSample-BridgingHeader.h
+//  PXGoogleDirectionsSample
+//
+//  Created by Romain on 11/09/2015.
+//  Copyright © 2015 Poulpix. All rights reserved.
+//
+
+#ifndef PXGoogleDirectionsSample_BridgingHeader_h
+#define PXGoogleDirectionsSample_BridgingHeader_h
+
+#include "GoogleMaps/GoogleMaps.h"
+
+#endif /* PXGoogleDirectionsSample_BridgingHeader_h */
